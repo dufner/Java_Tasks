c5d1a6f14b85d4f4e3466c731022d70d4cee6f6b
dufner <45033457+dufner@users.noreply.github.com>
2024-06-13 17:28:36 +0300

    Add files via upload

---
+++ java_tasks/src/tasks/Task1.java
@@ -0,0 +1,17 @@
+//: object/E01_DefaultInitialization.java
+/****************** Exercise 1 *****************
+ * Create a class containing an int and a char
+ * that are not initialized. Print their values
+ * to verify that Java performs default
+ * initialization.
+ ***********************************************/
+package tasks;
+
+public class Task1{
+
+
+
+} /* Output:
+i = 0
+c = [ ]
+*///:~
