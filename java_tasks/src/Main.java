89757bdba4c245fe845a7c1ccc90d89221bdd5af
vik <dufner@mail.ru>
2024-06-13 18:12:04 +0300

    1 commit

--- java_tasks/src/Main.java
+++ java_tasks/src/Main.java
@@ -1,21 +1,16 @@
+import tasks.Task1;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(countDuplicateCharacters("dfdf"));
+
+        Task1 task1= new Task1();
+        System.out.println(task1.countDuplicateCharacters("dfdf"));
     }
 
-    public static Map<Character,Integer> countDuplicateCharacters(String str){
-        Map<Character,Integer> result= new HashMap<>();
-        for (int i=0;i<str.length();i++){
-            char ch =str.charAt(i);
-            result.compute(ch,(k,v)->(v==null)?1 :++v);
-        }
 
-
-        return result;
-    }
 
 
 }
--- java_tasks/src/tasks/Task1.java
+++ java_tasks/src/tasks/Task1.java
@@ -7,9 +7,22 @@
  ***********************************************/
 package tasks;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task1{
 
+    public static Map<Character,Integer> countDuplicateCharacters(String str){
+        Map<Character,Integer> result= new HashMap<>();
+        for (int i=0;i<str.length();i++){
+            char ch =str.charAt(i);
+            result.compute(ch,(k,v)->(v==null)?1 :++v);
+        }
 
+
+        Map<Character, Integer> resulthjh;
+        return resulthjh;
+    }
 
 } /* Output:
 i = 0
